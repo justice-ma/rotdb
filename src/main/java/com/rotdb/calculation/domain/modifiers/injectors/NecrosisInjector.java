08281f4e798c88d1c9d84ad7309b2265a1d9ad8a
justice-ma <justice.mazerolle@edu.sait.ca>
2026-04-09 22:27:00 -0600

    Fix: Stupid.

--- src/main/java/com/rotdb/calculation/domain/modifiers/injectors/NecrosisInjector.java
+++ src/main/java/com/rotdb/calculation/domain/modifiers/injectors/NecrosisInjector.java
@@ -3,7 +3,6 @@ package com.rotdb.calculation.domain.modifiers.injectors;
 import com.rotdb.calculation.domain.model.context.AbilityHitsContext;
 import com.rotdb.calculation.domain.model.context.CalculationContext;
 import com.rotdb.calculation.domain.model.enums.BuffId;
-import com.rotdb.calculation.domain.model.enums.CombatStyles;
 import com.rotdb.calculation.domain.modifiers.Modifier;
 import com.rotdb.shared.ability.AbilityId;
 
@@ -11,15 +10,12 @@ import java.util.List;
 
 public class NecrosisInjector implements Modifier {
     public void apply(CalculationContext context) {
-        if (context.getEquipment().getCombatStyle() != CombatStyles.NECROMANCY || !context.getBuffs().has(BuffId.NECROSIS)
-            || context.getBuffs().stacks(BuffId.NECROSIS) <= 0 || context.getAbility().getId() != AbilityId.DEATHGRASP) {
-            return;
+        if (context.getAbility().getId() == AbilityId.DEATHGRASP && context.getBuffs().has(BuffId.NECROSIS)) {
+            List<AbilityHitsContext> hit = context.getAbility().getHits();
+            double min = hit.getFirst().getMin() + context.getBuffs().stacks(BuffId.NECROSIS) * 0.4;
+            double max = hit.getFirst().getMax() + context.getBuffs().stacks(BuffId.NECROSIS) * 0.4;
+            hit.getFirst().setMin(min);
+            hit.getFirst().setMax(max);
         }
-
-        List<AbilityHitsContext> hit = context.getAbility().getHits();
-        double min = hit.getFirst().getMin() + context.getBuffs().stacks(BuffId.NECROSIS) * 0.4;
-        double max = hit.getFirst().getMax() + context.getBuffs().stacks(BuffId.NECROSIS) * 0.4;
-        hit.getFirst().setMin(min);
-        hit.getFirst().setMax(max);
     }
 }
