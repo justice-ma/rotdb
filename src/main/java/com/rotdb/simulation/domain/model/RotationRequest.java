99aea57626df520fb89da5e21f2177c32295d40f
justice-ma <justice.mazerolle@edu.sait.ca>
2026-04-11 00:01:47 -0600

    Stop tracking simulation package

--- src/main/java/com/rotdb/simulation/domain/model/RotationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rotdb.simulation.domain.model;
-
-import com.rotdb.shared.ability.AbilityId;
-import com.rotdb.simulation.domain.model.context.AbilityCooldownContext;
-
-public class RotationRequest {
-    private AbilityId abilityId;
-    private AbilityCooldownContext abilityCooldowns;
-}
